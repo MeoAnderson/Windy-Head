a89bcf4484e6ebed077284b5c768569d89be7894
MeoAnderson <vrpoo@iscte.pt>
2014-06-22 01:57:45 +0100

    Task List + Calendar
    - Adding tasks and editing them is now implemented;
    - All tasks are added automatically to the Calendar app, via EventKit.
    
    TODO:
    Dismiss modal view if a user edits an entry.

---
+++ Windy Head/TasksViewController.h
@@ -0,0 +1,16 @@
+//
+//  TasksViewController.h
+//  Windy Head
+//
+//  Created by Vasco Patrício on 21/06/14.
+//  Copyright (c) 2014 ISCTE-IUL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <EventKit/EventKit.h>
+#import <EventKitUI/EventKitUI.h>
+
+@interface TasksViewController : UITableViewController
+
+
+@end
